1622d6750c96ddb742ef11095034e595d13393fa
Agustin Vidal <agustinvm88@gmail.com>
2025-03-27 10:20:12 +0000

    Add modifications

--- handle_format.c
+++ handle_format.c
@@ -3,49 +3,49 @@
 #include <unistd.h>
 
 /**
- * handle_format - Maneja los especificadores de formato y llama a la función correspondiente
- * @format: Especificador de formato
- * @args: Lista de argumentos
+ * handle_format - Maneja los formatos dentro de la cadena.
+ * @specifier: Especificador de formato.
+ * @args: Lista de argumentos para ser imprimidos.
  *
- * Return: Número de caracteres impresos
+ * Return: Número de caracteres impresos.
  */
-int handle_format(char format, va_list args)
+int handle_format(char specifier, va_list args)
 {
     int count = 0;
 
-    switch (format)
+    if (specifier == 'c')
     {
-        case 'c':
-            count += print_char(va_arg(args, int));
-            break;
-        case 's':
-            count += print_string(va_arg(args, char *));
-            break;
-        case 'd':
-        case 'i':
-            count += print_number(va_arg(args, int), 10, 0);
-            break;
-        case 'u':
-            count += print_number(va_arg(args, unsigned int), 10, 0);
-            break;
-        case 'o':
-            count += print_number(va_arg(args, unsigned int), 8, 0);
-            break;
-        case 'x':
-            count += print_number(va_arg(args, unsigned int), 16, 0);
-            break;
-        case 'X':
-            count += print_number(va_arg(args, unsigned int), 16, 1);
-            break;
-        case 'p':
-            count += print_pointer(va_arg(args, void *));
-            break;
-        case '%':
-            count += _putchar('%');
-            break;
-        default:
-            count += _putchar('%') + _putchar(format);
-            break;
+        char c = va_arg(args, int);
+        count += print_char(c);
     }
-    return (count);
+    else if (specifier == 's')
+    {
+        char *str = va_arg(args, char *);
+        count += print_string(str);
+    }
+    else if (specifier == 'd' || specifier == 'i')
+        count += print_number(va_arg(args, int), 10, 0);
+    else if (specifier == 'u')
+        count += print_number(va_arg(args, unsigned int), 10, 0);
+    else if (specifier == 'o')
+        count += print_number(va_arg(args, unsigned int), 8, 0);
+    else if (specifier == 'x')
+        count += print_number(va_arg(args, unsigned int), 16, 0);
+    else if (specifier == 'X')
+        count += print_number(va_arg(args, unsigned int), 16, 1);
+    else if (specifier == 'p')
+        count += print_pointer(args);
+    else if (specifier == '%')
+    {
+        _putchar('%');
+        count++;
+    }
+    else
+    {
+        _putchar('%');
+        _putchar(specifier);
+        count += 2;
+    }
+
+    return count;
 }
